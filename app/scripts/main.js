b3460866a7f436ea3934bd6b64799bc32f0f4ea9
Nkululeko Monde Sineke <nsineke@gmail.com>
2017-04-10 21:17:16 +0200

    Scroll down button functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+let doScrollDown = (destinationEl, scrollingEl = '#js-doc-body') => {
+  const scrollDestination = document.querySelector(destinationEl).scrollHeight;
+
+  $(scrollingEl).animate({scrollTop: scrollDestination}, '500', 'swing');
+}
+
+document.querySelector('#js-btn-scroll-down').addEventListener('click', function() {
+  doScrollDown('#app-page');
+});
