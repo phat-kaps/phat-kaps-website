21476cd9903535061069a72bd65a3fe53c9ee602
Nkululeko Monde Sineke <nsineke@gmail.com>
2017-04-12 00:02:56 +0200

    Fix scroll button function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
-let doScrollDown = (destinationEl, scrollingEl = '#js-doc-body') => {
-  const scrollDestination = document.querySelector(destinationEl).scrollHeight;
+$(document).ready(function (){
+  let doScrollDown = (destinationEl, scrollingEl = 'body') => {
+    const scrollDestination = document.querySelector(destinationEl).scrollHeight;
+    $(scrollingEl).animate({scrollTop: scrollDestination}, '500', 'swing');
+  }
 
-  $(scrollingEl).animate({scrollTop: scrollDestination}, '500', 'swing');
-}
-
-document.querySelector('#js-btn-scroll-down').addEventListener('click', function() {
-  doScrollDown('#app-page');
+  document.querySelector('#js-btn-scroll-down').addEventListener('click', function() {
+    doScrollDown('#app-page');
+  });
 });
